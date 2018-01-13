119ae30d62d234c1bbd1ec7fd5f16f79caf64728
Alex Hoffman <alexhoffman617@gmail.com>
2018-01-12 23:36:13 -0500

    League leaders functionality

--- src/app/components/league-home/league-home.component.ts
+++ src/app/components/league-home/league-home.component.ts
@@ -5,6 +5,7 @@ import { GamePlayer } from '../../models/game';
 import { Team } from '../../models/team';
 import { Player } from '../../models/player';
 import { LeagueDataService } from '../../services/league-data.service';
+import { SharedFunctionsService } from '../../services/shared-functions.service';
 import { PlayGameService } from '../../services/play-game.service';
 import { LeagueProgressionService } from '../../services/league-progression.service';
 import { StaticListsService } from '../../services/static-lists.service';
@@ -19,13 +20,17 @@ import 'rxjs/add/operator/first'
 export class LeagueHomeComponent implements OnInit {
   restOfSeason = 'ROS'
   leagueId;
+  players: Array<Player>;
   constructor(private route: ActivatedRoute,
               public leagueDataService: LeagueDataService,
+              public sharedFunctionsService: SharedFunctionsService,
               public staticListsService: StaticListsService,
   ) { }
 
   async ngOnInit() {
-
+    this.leagueDataService.playersObservable.subscribe(players => {
+      this.players = players
+    })
   }
 
   getWins(teamId) {
@@ -79,5 +84,35 @@ export class LeagueHomeComponent implements OnInit {
       resultArray.push(that.leagueDataService.getTeamById(id))
     })
     return resultArray
+  }
+
+  getLeaders(attribute: string, isSharedFunction: boolean = false, isPitching: boolean = false, asc: boolean = false) {
+    const that = this
+    const filteredPlayers = _.filter(this.players, function(player){
+      if (isPitching) {
+        return that.getStat(player, 'innings', false, true) > 0
+      } else {
+        return that.getStat(player, 'plateAppearences', false, false) > 0
+      }
+    })
+    return _.orderBy(filteredPlayers, function(player){
+     return that.getStat(player, attribute, isSharedFunction, isPitching)
+    }, asc ? 'asc' : 'desc').slice(0, 3)
+  }
+
+  getStat(player: Player, attribute: string, isSharedFunction: boolean, isPitching: boolean = false) {
+    const that = this
+    let stats
+    if (isPitching) {
+      stats = _.find(player.pitchingSeasonStats, function(s){
+        return s.year === that.leagueDataService.currentSeason.year
+      })
+    } else {
+      stats = _.find(player.hittingSeasonStats, function(s){
+        return s.year === that.leagueDataService.currentSeason.year
+      })
+    }
+
+    return isSharedFunction ? that.sharedFunctionsService[attribute](stats) : stats[attribute]
   }
 }
